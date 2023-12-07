f6c8a8da2c37cf2b1ac61ce9e041b57d023b05f7
Lukasz <lukaszprzybylka@gmail.com>
2023-12-07 11:10:49 +0000

    feat: add networking resources

--- index.ts
+++ index.ts
@@ -1,12 +1,84 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 
-let config = new pulumi.Config();
-let cidr = config.require("cidr");
-let instance_type = config.require("instance_type");
-let public_subnets = config.require("public_subnets");
-let private_subnets = config.require("private_subnets");
-let azs = config.require("azs");
-console.log(Array.isArray(azs));
+interface vpc {
+  name: string;
+  cidr: string;
+  azs: string[];
+  public_subnets: string[];
+  private_subnets: string[];
+}
 
-export const zones = azs;
+let config = new pulumi.Config();
+let vpc = config.requireObject<vpc>("vpc");
+
+const main = new aws.ec2.Vpc("main", {
+  cidrBlock: vpc.cidr,
+  enableDnsHostnames: true,
+  enableDnsSupport: true,
+
+  tags: {
+    Name: vpc.name,
+  },
+});
+
+const pub_subs = vpc.public_subnets.map(
+  (cidr, index) =>
+    new aws.ec2.Subnet(`public-${index}`, {
+      vpcId: main.id,
+      cidrBlock: cidr,
+      availabilityZone: vpc.azs[index],
+      mapPublicIpOnLaunch: true,
+
+      tags: {
+        Name: `public-sub-${index}`,
+      },
+    })
+);
+
+const pri_subs = vpc.private_subnets.map(
+  (cidr, index) =>
+    new aws.ec2.Subnet(`private-${index}`, {
+      vpcId: main.id,
+      cidrBlock: cidr,
+      availabilityZone: vpc.azs[index],
+      mapPublicIpOnLaunch: false,
+
+      tags: {
+        Name: `private-sub-${index}`,
+      },
+    })
+);
+
+const igw = new aws.ec2.InternetGateway("gw", {
+  vpcId: main.id,
+
+  tags: {
+    Name: `${vpc.name}-igw`,
+  },
+});
+
+const pub_rt = new aws.ec2.RouteTable("public", {
+  vpcId: main.id,
+
+  routes: [
+    {
+      cidrBlock: "0.0.0.0/0",
+      gatewayId: igw.id,
+    },
+  ],
+
+  tags: {
+    Name: "public-route-table",
+  },
+});
+
+const pub_rt_association = pub_subs.map(
+  (subnet, index) =>
+    new aws.ec2.RouteTableAssociation(`routeTableAssociation-${index}`, {
+      subnetId: subnet.id,
+      routeTableId: pub_rt.id,
+    })
+);
+
+// export const public_subnets = pub_subs;
